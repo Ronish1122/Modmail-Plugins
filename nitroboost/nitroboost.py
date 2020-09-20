8abc5a656610b0d1db72449d419bc4f7be83b325
Ronish1122 <55736291+Ronish1122@users.noreply.github.com>
2020-09-20 12:15:49 +0530

    Update nitroboost.py

--- nitroboost/nitroboost.py
+++ nitroboost/nitroboost.py
@@ -9,9 +9,9 @@ class BoostPlugin(commands.Cog):
 
         print(message.type)
         if message.type == discord.MessageType.premium_guild_subscription:
-            embed = discord.Embed(title=f"**Nitro Boost**", description=f"Thank you so much for boosting <a:BoostingAnimated:707512475246919712> !", color=0xff0000)
+            embed = discord.Embed(title=f"**Nitro Boost**", description=f"Thank you so much for boosting <a:gifhb_nitro:756561976020172910> !", color=0xff0000)
             m = await message.channel.send(embed=embed)
-            await m.add_reaction("<a:Heartanimated:707510241276985425>")
+            await m.add_reaction("<a:gifhb_heart:756561858705752134>")
             
 def setup(bot):
     bot.add_cog(BoostPlugin(bot))
